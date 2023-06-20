f53c8fb3ce24a093e3a6c87ed63c7cf4e0fdb3cb
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-06-20 09:42:34 +0330

    form styled

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ import React from "react";
 
 const Form = ({ onClick }) => {
   return (
-    <div>
+    <div className="Form">
       <form className="border p-4 mx-5 my-3 rounded shadow">
         <div className="d-flex justify-content-end">
           <FaTimes
@@ -14,14 +14,14 @@ const Form = ({ onClick }) => {
             onClick={onClick}
           />
         </div>
-        <h2>فرم مشخصات</h2>
+        <h2 className="mb-5">فرم مشخصات</h2>
         <div className="container">
           <div className="row">
-            <div className="col-2">
+            <div className="col-4">
               <label className="col align-self-start" />
               نام
             </div>
-            <div className="col-10">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder="نام"
@@ -35,134 +35,121 @@ const Form = ({ onClick }) => {
               <label className="col align-self-start" />
               نام خانوادگی
             </div>
-            <div className="col-8">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder=" نام خانوادگی"
-                autoFocus
                 className="form-control"
               ></input>
             </div>
           </div>
           <div className="row">
-            <div className="col-2">
+            <div className="col-4">
               <label className="col align-self-start" />
               نام مادر
             </div>
-            <div className="col-10">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder="نام مادر"
-                autoFocus
                 className="form-control"
               ></input>
             </div>
           </div>
           <div className="row">
-            <div className="col-2">
+            <div className="col-4">
               <label className="col align-self-start" />
               نام پدر
             </div>
-            <div className="col-10">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder="نام پدر"
-                autoFocus
                 className="form-control"
               ></input>
             </div>
           </div>
           <div className="row">
-            <div className="col-3">
+            <div className="col-4">
               <label className="col align-self-start" />
               نام برادر 1
             </div>
-            <div className="col-9">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder="نام برادر 1"
-                autoFocus
                 className="form-control"
               ></input>
             </div>
           </div>
           <div className="row">
-            <div className="col-3">
+            <div className="col-4">
               <label className="col align-self-start" />
               نام برادر 2
             </div>
-            <div className="col-9">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder="نام برادر 2"
-                autoFocus
                 className="form-control"
               ></input>
             </div>
           </div>
           <div className="row">
-            <div className="col-3">
+            <div className="col-4">
               <label className="col align-self-start" />
               نام برادر 3
             </div>
-            <div className="col-9">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder="نام برادر 3"
-                autoFocus
                 className="form-control"
               ></input>
             </div>
           </div>
           <div className="row">
-            <div className="col-3">
+            <div className="col-4">
               <label className="col align-self-start" />
               نام خواهر 1
             </div>
-            <div className="col-9">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder="نام خواهر 1"
-                autoFocus
                 className="form-control"
               ></input>
             </div>
           </div>
           <div className="row">
-            <div className="col-3">
+            <div className="col-4">
               <label className="col align-self-start" />
               نام خواهر 2
             </div>
-            <div className="col-9">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder="نام خواهر 2"
-                autoFocus
                 className="form-control"
               ></input>
             </div>
           </div>
           <div className="row">
-            <div className="col-3">
+            <div className="col-4">
               <label className="col align-self-start" />
               نام خواهر 3
             </div>
-            <div className="col-9">
+            <div className="col-7">
               <input
                 type="text"
                 placeholder="نام خواهر 3"
-                autoFocus
                 className="form-control"
               ></input>
             </div>
           </div>
 
           <div>
-            <input
-              type="submit"
-              className="btn btn-dark mt-3"
-              value={"ادامه"}
-            ></input>
+            <input type="submit" className="btn mt-3" value={"ادامه"}></input>
           </div>
         </div>
       </form>
